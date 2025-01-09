a2651e89eb21e84b78626b30f4254fbbeaa04e28
thibautdegbetchi <thibautdegbetchi@gmail.com>
2025-01-09 20:53:22 +0100

    initilization of the project

---
+++ src/test/java/training/test/demo/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package training.test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
